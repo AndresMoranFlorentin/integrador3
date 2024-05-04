305f37e50647f3af18271e670f4671233b76cf08
Lautaro Lamenza <laulamenza22@live.com.ar>
2024-05-03 22:33:28 -0300

    muevo las clases repositorio a la carpeta repositories

--- src/main/java/com/example/integrador_3/repositories/EstudianteRepo.java
+++ src/main/java/com/example/integrador_3/repositories/EstudianteRepo.java
@@ -1,8 +1,8 @@
-package com.example.integrador_3.repositorios;
+package com.example.integrador_3.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarreraRepo extends JpaRepository {
+public interface EstudianteRepo extends JpaRepository {
 }

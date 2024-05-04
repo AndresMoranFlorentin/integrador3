daeeba59d2a2cacb332a2324e5bb37a807e3a115
AndresMoranFlorentin <moranandres729@gmail.com>
2024-05-03 22:05:31 -0300

    se agregan la carpeta repositorio que controlara las operaciones rest y la entidad

---
+++ src/main/java/com/example/integrador_3/repositorios/CarreraEstudianteRepo.java
@@ -0,0 +1,8 @@
+package com.example.integrador_3.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarreraEstudianteRepo extends JpaRepository {
+}
